b1edc65f8b7800c8a110f60bc01cad88dff7adf9
Nohdajeong <dajeong0404@naver.com>
2023-06-16 13:33:10 +0900

    230616 - 02
    AbstractFactory.h 추가

---
+++ Jusin-1945/AbstractFactory.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "CObj.h"
+
+template<typename T>
+class CAbstractFactory {
+public:
+	CAbstractFactory() {};
+	~CAbstractFactory() {};
+
+public:
+	static CObj* Create() {
+		CObj* pObj = new T;
+		pObj->Initialize();
+
+		return pObj;
+	}
+
+	static CObj* Create(float _fX, float _fY) {
+		CObj* pObj = new T;
+		pObj->Initialize();
+		pObj->Set_Pos(_fX, _fY);
+
+		return pObj;
+	}
+};
--- Jusin-1945/CMainGame.cpp
+++ Jusin-1945/CMainGame.cpp
@@ -1,5 +1,6 @@
 #include "framework.h"
 #include "CMainGame.h"
+#include "AbstractFactory.h"
 
 CMainGame::CMainGame()
 {
